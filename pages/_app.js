e2fbaeae9ffe9cb4cf5e88f1787cd519f453d44e
mcclintsayshi <manakanejustin@gmail.com>
2022-07-01 10:55:48 +0200

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
+import Script from 'next/script'
 import '../styles/globals.css'
 
+const analyticsID = process.env.NEXT_PUBLIC_ANALYTICS_ID
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return 
+  <>
+    <Script
+        id="10"
+        src={`https://www.googletagmanager.com/gtag/js?id=${analyticsID}`}
+      />
+      <Script id="20">
+        {`window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', '${analyticsID}');`}
+    </Script>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
